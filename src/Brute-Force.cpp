6e516ee370492f6cb09d5c03e9eb1d358cc580ab
Pavel Semkin <pvsemk@mail.ru>
2019-03-25 21:42:17 +0300

    Variables renamed

--- src/Brute-Force.cpp
+++ src/Brute-Force.cpp
@@ -3,19 +3,20 @@
 //
 #include "Brute-Force.h"
 
-std::vector<int> bruteForceSearch(std::string s, std::string t)
+std::vector<int> bruteForceSearch(std::string txt, std::string pat)
 {
     std::vector<int> result;
     // Check all possible shifts
-    for (int i(0); i <= s.length() - t.length(); i++)
+    for (int i(0); i <= txt.length() - pat.length(); i++)
     {
         int j(i);
-        // Check equality of s[i...i + t.length - 1] and template
-        for (int k(0); j < i + t.length(); j++, k++)
-            if (s[j] != t[k])
+        // Check equality of txt[i...i + pat.length - 1] and template
+        for (int k(0); j < i + pat.length(); j++, k++)
+            if (txt[j] != pat[k])
                 break;
-        // Add a current shift into the result if all the symbols are equal
-        if (j - i == t.length())
+
+        // Add a current shift into the result only if all the symbols are equal
+        if (j - i == pat.length())
         {
             result.push_back(i);
         }
--- src/Karp-Rabin.cpp
+++ src/Karp-Rabin.cpp
@@ -7,30 +7,44 @@
 
 
 
-std::vector<int> searchKarpRabin(std::string s, std::string t, int q) // s - search, t - template, q - prime number
+std::vector<int> searchKarpRabin(std::string txt, std::string pat, int q) // q - prime number
 {
     std::vector<int> result;
     int h(1);
 
     // Count h variable in order to recount hash later
     // (It will help to remove the leading symbol)
-    // Formula: h = pow(d, t.length() - 1) % q
-    for (int i(0); i < t.length() - 1; i++)
+    // Formula: h = pow(d, pat.length() - 1) % q
+    for (int i(0); i < pat.length() - 1; i++)
         h = d * h % q;
 
     int thash(0), shash(0); // hash value for the template and string
 
     // Calculate value for the template and the first windows of the string
-    for (int i(0); i < t.length(); i++)
+    for (int i(0); i < pat.length(); i++)
     {
-        thash = (d*thash + t[i])%q;
-        shash = (d*shash + s[i])%q;
+        thash = (d * thash + pat[i]) % q;
+        shash = (d * shash + txt[i]) % q;
     }
 
     // Check all possible shifts
-    for (int i(0); i <= s.length() - t.length(); i++)
+    for (int i(0), j; i <= txt.length() - pat.length(); i++)
     {
+        // Check hash values of the current window and the template
+        // If they are equal, compare all symbols in order to exclude collision-cases
+        if (shash == thash)
+        {
+            // Check equality of txt[i...i + pat.length - 1] and template
+            for (j = 0; j < pat.length(); j++)
+                if (txt[j + i] != pat[j])
+                    break;
 
+            // Add a current shift into the result only if all the symbols are equal
+            if (j == pat.length())
+            {
+                result.push_back(i);
+            }
+        }
     }
 
 

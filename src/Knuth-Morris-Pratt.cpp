89c6fe37e2469a9d045ea2ccbd79ca2407da9ec5
Pavel Semkin <pvsemk@mail.ru>
2019-03-25 23:07:21 +0300

    Update tests.cpp

--- src/Knuth-Morris-Pratt.cpp
+++ src/Knuth-Morris-Pratt.cpp
@@ -1,4 +1,4 @@
 //
 // Created by pavel on 25.03.19.
 //
-#include "Knuth-Morris-Pratt.cpp"
+#include "Knuth-Morris-Pratt.h"
--- src/main3.cpp
+++ src/main3.cpp
@@ -1,4 +1,7 @@
 //
 // Created by pavel on 25.03.19.
 //
-
+int main()
+{
+    return 0;
+}
